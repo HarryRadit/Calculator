d5fc991c7b2633be256fe4415d9f41e5beec6dee
HarryRadit <harrydxm@gmail.com>
2024-10-27 19:21:33 +0700

    kaluator

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def calculator():
-    return render_template('calculatory.html')
+    result = None
 
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+
+
+        if operation == 'add':
+            result = int(num1) + int(num2)
+        elif operation == 'subtract':
+            result = int(num1) - int(num2)
+        elif operation == 'multiply':
+            result = int(num1) * int(num2)
+        elif operation == 'divide':
+            result = int(num1) / int(num2)
+
+    return render_template('calculatory.html', result=result)
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
